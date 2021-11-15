1806878feb696f6c2f4a213c08ba6a71b8dbe596
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-15 17:23:36 +0530

    Adding about Page and AboutInfoItem Component

---
+++ src/components/AboutInfoItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import PText from './PText';
+
+const AboutItemStyles = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-top: 3rem;
+  .title {
+    font-size: 2.4rem;
+  }
+  .items {
+    display: flex;
+    gap: 1.5rem;
+    position: absolute;
+    left: 18rem;
+  }
+  .item {
+    background-color: var(--deep-dark);
+    padding: 1rem;
+    border-radius: 8px;
+  }
+`;
+
+export default function AboutInfoItem({
+  title = 'This is Title',
+  items = ['HTML', 'CSS', 'JS'],
+}) {
+  return (
+    <AboutItemStyles>
+      <div className="title">{title}</div>
+      <div className="items">
+        {items.map((item, index) => (
+          <div className="item" key={index}>
+            <PText>{item}</PText>
+          </div>
+        ))}
+      </div>
+    </AboutItemStyles>
+  );
+}
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,92 @@
 import React from 'react';
+import styled from 'styled-components';
+import PText from '../components/PText';
+import Button from '../components/Button';
+import AboutImg from '../assets/images/about-page-img.png';
+import AboutInfoItem from '../components/AboutInfoItem';
+
+const AboutPageStyles = styled.div`
+  padding: 20rem 0 10rem 0;
+  .top-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 2rem;
+  }
+  .left {
+    flex: 3;
+  }
+  .right {
+    flex: 2;
+  }
+  .about__subheading {
+    font-size: 2.2rem;
+    margin-bottom: 2rem;
+    span {
+      background-color: var(--deep-dark);
+      padding: 0.5rem;
+      border-radius: 8px;
+    }
+  }
+  .about__heading {
+    font-size: 3.6rem;
+    margin-bottom: 3rem;
+  }
+  .about__info {
+    margin-bottom: 4rem;
+    .para {
+      max-width: 100%;
+    }
+  }
+  .right {
+    img {
+      border: 2px solid var(--gray-1);
+    }
+  }
+`;
 
 export default function About() {
   return (
-    <div>
-      <h1> This is About Page</h1>
-    </div>
+    <AboutPageStyles>
+      <div className="container">
+        <div className="top-section">
+          <div className="left">
+            <p className="about__subheading">
+              Hi, I am <span> Dibya Ranjan </span>
+            </p>
+
+            <h2 className="about__heading">A UI Developer</h2>
+            <div className="about__info">
+              <PText>
+                I am from Bengaluru, Karnataka, India. Since my childhood I have
+                always loved Art and Design. I love to create things that are
+                useful to others.
+                <br />
+                <br />I started programming since I was doing my Bachelors in
+                Computer Science and Engineering. I loved it and now I have the
+                opportunity to create beautiful and aesthetic user interfaces. I
+                find it really interesting and I enjoy this process a lot.
+                <br />
+                <br />
+                My Vision is to make the world a better place. Now almost
+                everything is becoming better than ever. It's time for use to
+                create beautiful things so that the world can be a better place.
+              </PText>
+            </div>
+            <Button btnText="Resume" btnLink="#" />
+          </div>
+          <div className="right">
+            <img src={AboutImg} alt="dibyaimg" />
+          </div>
+        </div>
+
+        <div className="about__info__items">
+          <div className="about__info__item">
+            <h1 className="about__info__heading">Education</h1>
+            <AboutInfoItem />
+          </div>
+        </div>
+      </div>
+    </AboutPageStyles>
   );
 }
